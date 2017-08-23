f8e44400307ffa6d04008b877463094f529cac65
greylovee <greylovee@gmail.com>
2017-08-23 09:48:21 -0500

    Add files via upload

---
+++ clase.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    /*
+    //EJERCICIO AÑOS BISIESTOS
+    int anho;
+    cout<<"ingrese el anho "<<endl;
+    cin>> anho;
+    if (anho%4 == 0 && (anho%100!= 0  || anho%4==0))
+        cout<<"anho bisiesto"<<endl;
+    else
+        cout<<"anho no bisiesto"<<endl;
+     */
+
+    //EJERCICIO NUMEROS PRIMOS
+    int num;
+    cout<<"ingrese el numero"<<endl;
+    cin>>num;
+    if (num%1== 0 && num%num ==0)
+        cout<<"el numero si es primo"<<endl;
+}
